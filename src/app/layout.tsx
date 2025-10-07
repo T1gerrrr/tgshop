fbeddda36d0926a8621cb7d96d492a0eb2382615
Tiger_Kon <hotrachkhoa123@gmail.com>
2025-10-07 11:31:25 +0700

    update logo 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import tigerIcon from "./images/icon_tiger.png";
+
 import "./globals.css";
 
 const geistSans = Geist({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,22 @@
 import styles from "./page.module.css";
 import logo from "./images/icon.png";
 import Image from "next/image";
+import Link from "next/link";
 export default function Home() {
   return (
     <div className={styles.page}>
       {/* Hero Section */}
       <section className={styles.hero}>
         <nav className={styles.navbar}>
-          <a href="/" className={styles.brand} aria-label="Trang chủ">
+          <Link href="/" className={styles.brand} aria-label="Trang chủ">
             <Image
-              src={logo}           
+              src={logo}
               alt=""
               width={55}
               height={50}
-              priority           
+              priority
             />
-          </a>
+          </Link>
           <ul className={styles.navLinks}>
             <li><a href="#products">Sản Phẩm</a></li>
             <li><a href="#contact">Liên Hệ</a></li>
